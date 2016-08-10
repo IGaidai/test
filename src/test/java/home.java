53e899622a0c7833858a9f577889e35dd798d977
IGaidai <ievgen.gaidai@gmail.com>
2016-08-11 00:00:22 +0200

    test

--- src/test/java/home.java
+++ src/test/java/home.java
@@ -11,6 +11,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.MarionetteDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static junit.framework.TestCase.assertEquals;
@@ -19,17 +23,15 @@ import static org.openqa.selenium.support.ui.ExpectedConditions.titleContains;
 @RunWith(SerenityRunner.class)
 public class home{
 
-    @Managed
-    WebDriver driver;
+//    @Managed
+//    WebDriver driver;
 
     @Test
     public void test() {
-        driver.get("http://www.google.com");
+        System.setProperty("webdriver.chrome.driver","C:\\dev\\chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.get("http://www.yahoo.com");
+        driver.close();
 
-        driver.findElement(By.name("q")).sendKeys("firefly", Keys.ENTER);
-
-//        new WebDriverWait(driver,15).until(titleContains("Google Search"));
-
-//        assertEquals(driver.getTitle(),"firefly - Google Search");
     }
 }
