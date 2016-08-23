7c3065af240f66426eaec28bae516739e3d1216c
IGaidai <ievgen.gaidai@gmail.com>
2016-08-23 22:31:20 +0200

    test

--- src/test/java/MiniMiniMusicApp.java
+++ src/test/java/MiniMiniMusicApp.java
@@ -1,33 +1,46 @@
 import javax.sound.midi.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MiniMiniMusicApp {
 
     public static void main(String[] args) {
-        MiniMiniMusicApp mini = new MiniMiniMusicApp();
-        mini.play();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            int instrument = Integer.parseInt(reader.readLine());
+            int note = Integer.parseInt(reader.readLine());
+            MiniMiniMusicApp mini = new MiniMiniMusicApp();
+            mini.play(instrument, note);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
     }
 
-    private void play() {
+    private void play(int instrument, int note) {
         try {
+
             Sequencer player = MidiSystem.getSequencer();
             player.open();
-
             Sequence seq = new Sequence(Sequence.PPQ, 4);
-
             Track track = seq.createTrack();
 
+            ShortMessage first = new ShortMessage();
+            first.setMessage(192, 1, instrument, 0);
+            MidiEvent changeInstrument = new MidiEvent(first, 1);
+            track.add(changeInstrument);
+
             ShortMessage a = new ShortMessage();
-            a.setMessage(144, 1, 44, 100);
+            a.setMessage(144, 1, note, 100);
             MidiEvent noteOn = new MidiEvent(a, 1);
             track.add(noteOn);
 
             ShortMessage b = new ShortMessage();
-            b.setMessage(128, 1, 44, 100);
+            b.setMessage(128, 1, note, 100);
             MidiEvent noteOff = new MidiEvent(b, 16);
             track.add(noteOff);
-
             player.setSequence(seq);
-
             player.start();
 
 
