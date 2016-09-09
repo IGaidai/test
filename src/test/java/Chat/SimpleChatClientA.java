ad8f4e6bab2e91e4d5159ffa943eb8c15514d1e8
IGaidai <ievgen.gaidai@gmail.com>
2016-09-09 23:32:31 +0200

    test

--- src/test/java/Chat/SimpleChatClientA.java
+++ src/test/java/Chat/SimpleChatClientA.java
@@ -1,8 +1,14 @@
 package Chat;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
+
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 public class SimpleChatClientA {
 
@@ -11,7 +17,36 @@ public class SimpleChatClientA {
     Socket sock;
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Simple Chat Client");
+        new SimpleChatClientA().go();
+    }
 
+    private void go() {
+        JFrame frame = new JFrame("Simple Chat Client");
+        JPanel mainPanel = new JPanel();
+        outgoing = new JTextField(20);
+        JButton sendButton = new JButton("Send");
+        sendButton.addActionListener(new SendButtonListener());
+        mainPanel.add(outgoing);
+        mainPanel.add(sendButton);
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        setUpNetworking();
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setSize(400, 500);
+        frame.setVisible(true);
+    }
+
+    private void setUpNetworking() {
+        try {
+            sock = new Socket("127.0.0.1", 5000);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private class SendButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+
+        }
     }
 }
