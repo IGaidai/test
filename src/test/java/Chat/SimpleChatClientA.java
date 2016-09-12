ee1b79f5b7929a9e2cadf8715032050abaacffe7
IGaidai <ievgen.gaidai@gmail.com>
2016-09-12 23:07:28 +0200

    test

--- src/test/java/Chat/SimpleChatClientA.java
+++ src/test/java/Chat/SimpleChatClientA.java
@@ -4,7 +4,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -12,8 +14,10 @@ import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 public class SimpleChatClientA {
 
+    JTextArea incoming;
     JTextField outgoing;
     PrintWriter writer;
+    BufferedReader reader;
     Socket sock;
 
     public static void main(String[] args) {
@@ -23,13 +27,27 @@ public class SimpleChatClientA {
     private void go() {
         JFrame frame = new JFrame("Simple Chat Client");
         JPanel mainPanel = new JPanel();
+        incoming = new JTextArea(15, 50);
+        incoming.setLineWrap(true);
+        incoming.setWrapStyleWord(true);
+        incoming.setEnabled(false);
+        JScrollPane qScroller = new JScrollPane(incoming);
+        qScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+
         outgoing = new JTextField(20);
         JButton sendButton = new JButton("Send");
         sendButton.addActionListener(new SendButtonListener());
+        mainPanel.add(qScroller);
         mainPanel.add(outgoing);
         mainPanel.add(sendButton);
         frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
         setUpNetworking();
+
+
+        Thread readerThread = new Thread(new IncomingReader());
+        readerThread.start();
+
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
         frame.setSize(400, 500);
         frame.setVisible(true);
@@ -39,6 +57,8 @@ public class SimpleChatClientA {
         try {
             sock = new Socket("127.0.0.1", 5000);
             writer = new PrintWriter(sock.getOutputStream());
+            InputStreamReader inputStreamReader = new InputStreamReader(sock.getInputStream());
+            reader = new BufferedReader(inputStreamReader);
             System.out.println("Connection established");
         } catch (IOException e) {
             e.printStackTrace();
@@ -59,5 +79,20 @@ public class SimpleChatClientA {
             outgoing.requestFocus();
         }
 
+    }
+
+    private class IncomingReader implements Runnable {
+        public void run() {
+            String message;
+
+            try {
+                while ((message = reader.readLine()) != null) {
+                    System.out.println("read " + message);
+                    incoming.append(message + "\n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
---
+++ src/test/java/Chat/VerySimpleChatServer.java
@@ -0,0 +1,7 @@
+package Chat;
+
+/**
+ * Created by Lenovo Z51-70 on 12.09.2016.
+ */
+public class VerySimpleChatServer {
+}
